ca2937f7539a2ac12f6756534ad8ceb036e0fbad
User3 <user3@example.com>
2026-01-28 16:24:21 +0530

    Break build intentionally

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     static  int add(int a, int b){
-        return a+b;
+        return a+;
     }
     public static void main(String[] args) {
             System.out.print(add(4,5));
